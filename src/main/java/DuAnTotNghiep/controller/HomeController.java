2296718e9d730591dce2a44c506ff9b39a15653c
Đức Tài <taindpc00961@fpt.edu.vn>
2021-09-25 20:04:04 +0700

    tai test

--- src/main/java/DuAnTotNghiep/controller/HomeController.java
+++ src/main/java/DuAnTotNghiep/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+//test Tai
 	@RequestMapping("/123")
 	public String test() {
 		return "../static/authorize/index";
