2bd6d2258af327baffead4f9771e8f6e153a68cc
HoanggSangg <125477569+HoanggSangg@users.noreply.github.com>
2025-02-20 12:17:36 +0700

    Update shopping-cart-app.js
    sua mot ti

--- src/main/resources/static/js/shopping-cart-app.js
+++ src/main/resources/static/js/shopping-cart-app.js
@@ -7,7 +7,7 @@ app.controller("shopping-cart-ctrl", function($scope, $http) {
 		add(id) {
 			$http.get(`/rest/products/${id}`).then(resp => {
 				var item = this.items.find(item => item.id == id);
-				alert("test github");
+				alert("test githubbb");
 				if (item) {
 					if (resp.data.soluong > item.qty) {
 						item.soluong--;
